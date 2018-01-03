1fe04637cd3dc9b20960fcf0374ff6b23816b2b7
azarovalex <alex@azarov.by>
2018-01-03 23:47:29 +0300

    Terminal blog finished. Yet it needs some fixes.

--- full-stack/terminal_blog/app.py
+++ full-stack/terminal_blog/app.py
@@ -1,16 +1,8 @@
 from Database import Database
-from Models.blog import Blog
+from menu import Menu
 
 Database.initialize()
 
-blog = Blog(author="alex",
-            title="Simple title",
-            description="Sample description")
+menu = Menu()
 
-blog.new_post()
-
-blog.save_to_mongo()
-
-from_database = Blog.get_from_mongo(blog.id)
-
-print(blog.get_posts())
+menu.run_menu()
--- full-stack/terminal_blog/menu.py
+++ full-stack/terminal_blog/menu.py
@@ -5,28 +5,45 @@ from Models.blog import Blog
 class Menu(object):
     def __init__(self):
         self.user = input("Enter your autor name: ")
+        self.user_blog = None
         if self._user_has_account():
             print("Welcome back {}".format(self.user))
         else:
             self._prompt_user_for_accout()
 
-            
-        pass
-
     def run_menu(self):
         read_or_write = input("Do you want to read (R) or write (W) blogs?")
         if read_or_write == 'R':
-            pass
+            self._list_blogs()
+            self._view_blog()
         elif read_or_write == "W":
-            pass
+            self.user_blog.new_post()
         else:
             print("Thank you for blogging!")
 
     def _user_has_account(self):
-        return Database.find_one("blogs", {"author": self.user}) is not None
+        blog = Database.find_one("blogs", {"author": self.user})
+        if blog is not None:
+            self.user_blog = Blog.get_from_mongo(blog['id'])
+            return True
+        else:
+            return False
 
     def _prompt_user_for_accout(self):
         title = input("Enter blog title: ")
         description = input("Enter blog description: ")
         blog = Blog(self.user, title, description)
         blog.save_to_mongo()
+        self.user_blog = blog
+
+    def _list_blogs(self):
+        blogs = Database.find('blogs', {})
+        for blog in blogs:
+            print("ID: {}, Title: {}, Author: {}".format(blog['id'], blog['title'], blog['author']))
+
+    def _view_blog(self):
+        blog_to_see = input("Enter the ID of the blog you would like to see: ")
+        blog = Blog.get_from_mongo(blog_to_see)
+        posts = blog.get_posts()
+        for post in posts:
+            print("Date: {}, Title: {}\n\n{}".format(post['created_date'], post['title'], post['content']))
